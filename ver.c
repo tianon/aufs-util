2dc4d00f74befe29655285a58425c9635bc45e95
J. R. Okajima <hooanon05g@gmail.com>
2019-10-02 23:01:01 +0900

    aufs5.0
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- ver.c
+++ ver.c
@@ -17,13 +17,23 @@
  */
 
 #include <linux/aufs_type.h>
+#undef NDEBUG
+#include <assert.h>
+#include <regex.h>
 #include <stdio.h>
 #include <string.h>
 #include "au_util.h"
 
 int main(int argc, char *argv[])
 {
-	if (!strncmp(AUFS_VERSION, "4.x-rcN", 7))
+	int err;
+	regex_t preg;
+	const char *pat = "^5\\."; /* aufs5.0 and later */
+
+	err = regcomp(&preg, pat, REG_EXTENDED | REG_NOSUB);
+	assert(!err); /* internal error */
+
+	if (!regexec(&preg, AUFS_VERSION, 0, NULL, 0))
 		return 0;
 
 	puts("Wrong version!\n"
